96f1d89d94ce5d24b13205e9af1b820dc528b624
Вергилий Двачевский <Vergils2ch@gmail.com>
2022-09-07 16:51:13 +0300

    загружаю программу с семинара

---
+++ Program.cs
@@ -0,0 +1,12 @@
+void Num()
+{
+  int n = Convert.ToInt32(Console.ReadLine());
+  int x = Convert.ToInt32(Console.ReadLine());
+if(n *n==x || x*x==n)
+    Console.WriteLine("yes");
+    else
+    Console.WriteLine("no");
+
+
+
+}
